fc7113c982176c9a428e6117b378c2419a060967
DmitRich <job.vladimir.spb@gmail.com>
2023-03-29 15:09:02 +0300

    1. Модульные тесты. [#159435]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class ConverterTest {
 
     @Test
-    public void rubleToEuro() {
+    public void whenConvert140RblThen2Euro() {
         float in = 140;
         float expected = 2;
         float out = Converter.rubleToEuro(in);
@@ -15,7 +15,7 @@ public class ConverterTest {
     }
 
     @Test
-    public void rubleToDollar() {
+    public void whenConvert180RblThen3Dlr() {
         float in = 180;
         float expected = 3;
         float out = Converter.rubleToDollar(in);
