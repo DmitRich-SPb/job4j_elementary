22c1338b63684178ba79f1eb49c4e94037372ab2
DmitRich <job.vladimir.spb@gmail.com>
2023-04-24 10:05:13 +0300

    20. Вывод значений двух переменных с поясняющими надписями

---
+++ src/main/java/ru/job4j/io/Task20.java
@@ -0,0 +1,12 @@
+package ru.job4j.io;
+
+public class Task20 {
+    public static void print(int x, int y) {
+        System.out.println("Переменные: " + x + " " + y);
+        System.out.println("Сумма: " + (x + y));
+        System.out.println("Произведение: " + (x * y));
+    }
+
+    public static void main(String[] args) {
+    }
+}
